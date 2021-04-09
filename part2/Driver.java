7465aa1088438f3f591e7c0fc4d0cb0153f0673c
Leonidas P. Papadakos <papadakospan@gmail.com>
2021-04-09 03:51:11 +0300

    add expr, cond, arg

--- part2/Driver.java
+++ part2/Driver.java
@@ -2,8 +2,7 @@ import java_cup.runtime.*;
 import java.io.*;
 
 class Driver {
-	public static void main(String[] argv) throws Exception{
-		System.err.println("Expression(s) separated by ';':");
+	public static void main(String[] argv) throws Exception {
 		Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
 		p.parse();
 	}
